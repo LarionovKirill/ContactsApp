420716e9f14eef2fb6d73f6f6403f9b6eef5e3c6
LarionovKirill <lari-kir2013@mail.ru>
2024-04-02 12:37:18 +0700

    feature(MainForm.cs): Изменено название главной формы приложения

---
+++ ContactAppUI/ContactAppUI/MainForm.cs
@@ -0,0 +1,19 @@
+using ContactApp.Model;
+using ContactApp.Service;
+
+namespace ContactAppUI
+{
+    /// <summary>
+    /// Класс главной формы приложения ContactApp.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Конструктор инициализации формы.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
